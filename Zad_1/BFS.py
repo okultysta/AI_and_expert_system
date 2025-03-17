eb6fa7ba62142362c80216f0bd4e358761730352
Jędrzej <251482@edu.p.lodz.pl>
2025-03-17 19:37:18 +0100

    BFS raczej gotowy, znajduje sciezke i sekwencje ruchów i wypisuje wszytsko

--- Zad_1/BFS.py
+++ Zad_1/BFS.py
@@ -1,24 +1,29 @@
 from collections import deque
 
-def solve_fbs(width, height, start_state):
+def solve_bfs(width, height, start_state):
     goal_state = tuple(range(1, width*height)) + (0,)
     states_to_check = deque([start_state])
-    checked = set([start_state])
-    parent_map = {start_state: 0}
+    visited = set([start_state])
+    parent_map = {start_state: None}
+    max_checked = 1000000000
+    checked = 0
 
-    while states_to_check:
+    while states_to_check and checked < max_checked:
         current_state = states_to_check.popleft()
 
         if current_state == goal_state:
-            return current_state
+            path = find_path(parent_map, current_state)
+            moves = get_moves_sequence(path, width)
+            return path, moves
 
         for neighbor in find_possible_moves(width, height, current_state):
-            if neighbor not in checked:
+            if neighbor not in visited:
                 states_to_check.append(neighbor)
-                checked.add(neighbor)
+                visited.add(neighbor)
                 parent_map[neighbor] = current_state
+                checked += 1
 
-    return None
+    return None, None
 
 
 
@@ -33,10 +38,41 @@ def find_possible_moves(width, height, state):
 
     for horizontal, vertical in moves:
         new_row, new_col = row + horizontal, col + vertical
-        if 0 <= new_row < width and 0 <= new_col < height:
+        if 0 <= new_row < height and 0 <= new_col < width:
             new_index = new_row * width + new_col
             new_state = state[:]
             new_state[new_index], new_state[empty_filed_index] = new_state[empty_filed_index], new_state[new_index]
-            neighbors.append(new_state)
+            neighbors.append(tuple(new_state))
 
     return neighbors
+
+def find_path(parent_map, state):
+    path = []
+    while state:
+        path.append(state)
+        state = parent_map[state]
+    path.reverse()
+    return path
+
+def get_moves_sequence(path, width):
+    moves = {
+        (0, 1): "U",
+        (0, -1): "D",
+        (1, 0): "R",
+        (-1, 0): "L",
+    }
+    moves_sequence = []
+    for i in range(len(path)-1):
+        current_state = path[i]
+        next_state = path[i+1]
+
+        current_index = current_state.index(0)
+        next_index = next_state.index(0)
+
+        row, col = divmod(current_index, width)
+        next_row, next_col = divmod(next_index, width)
+
+        move = (next_row - row, next_col - col)
+        moves_sequence.append(moves[move])
+
+    return moves_sequence
--- Zad_1/main.py
+++ Zad_1/main.py
@@ -1,7 +1,16 @@
+import random as rnd
+from BFS import solve_bfs
 
-state = tuple(range(1, 6)) + (0,)
+numbers = list(range(0, 9))
+rnd.shuffle(numbers)
+state = tuple(numbers)
+
+path, moves = solve_bfs(3,3,state)
+
+if path is not None:
+    for board in path:
+        print(board)
+    print()
 
 print(state)
-state = list(state)
-state[1], state[2] = state[2], state[1]
-print(state)
+print(" ".join(moves))
