65d3a589433917ffe0ce016ce52efc47c2add4b8
Kacperux1 <kacper34505@o2.pl>
2025-04-06 17:50:05 +0200

    {}

--- Zad_1/main.py
+++ Zad_1/main.py
@@ -32,38 +32,6 @@ state = (5, 1, 3, 4,
 
 state = tuple(state)
 
-# moves_dfs = solve_dfs(width, height, state, max_depth)
-#
-# if moves_dfs is not None:
-#     print("(DFS): Solution found in", len(moves_dfs))
-#     print(" ".join(moves_dfs))
-# else:
-#     print("Combination not found(DFS)")
-#
-# moves_bfs = solve_bfs(width, height, state, max_depth)
-#
-# if moves_bfs is not None:
-#     print("(BFS): Solution found in", len(moves_bfs))
-#     print(" ".join(moves_bfs))
-# else:
-#     print("Combination not found(BFS)")
-#
-# moves_hamming = heuristics.solve_heuristics(width, height, state, max_depth, heuristics.calculate_hamming)
-#
-# if moves_hamming is not None:
-#     print("(Hamming): Solution found in", len(moves_hamming))
-#     print(" ".join(moves_hamming))
-# else:
-#     print("Combination not found(Hamming)")
-#
-# moves_manhattan = heuristics.solve_heuristics(width, height, state, max_depth, heuristics.calculate_manhattan)
-#
-# if moves_manhattan is not None:
-#     print("(Manhattan): Solution found in", len(moves_manhattan))
-#     print(" ".join(moves_manhattan))
-# else:
-#     print("Combination not found(Manhattan)")
-
 
 litery_male = 'rdul'
 litery_duze = 'RDUL'
@@ -102,7 +70,7 @@ if algorithm == "astr":
     elif add_param == "manh":
         moves_manhattan = heuristics.solve_heuristics(width, height, state, max_depth, heuristics.calculate_manhattan, solution_file, info_file)
         if moves_manhattan is not None:
-            print("(Manhattan): Solution found in", len(moves_manhattan))
+            print("(Manhattan): Solution found in", len(moves_manhattan), " moves")
             print(" ".join(moves_manhattan))
         else:
             print("Combination not found(Manhattan)")
